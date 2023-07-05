e58f0fce25018bee7798f4e4f9549ce61bfa59d8
Andrew Chou <andrewchou@fastmail.com>
2023-07-05 16:38:45 -0400

    update service vite config to build in node environment

--- vite.service.config.mjs
+++ vite.service.config.mjs
@@ -3,6 +3,11 @@ import typescript from '@rollup/plugin-typescript'
 
 // https://vitejs.dev/config
 export default defineConfig({
+  resolve: {
+    // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
+    browserField: false,
+    mainFields: ['module', 'jsnext:main', 'jsnext'],
+  },
   build: {
     outDir: '.vite/build/service',
   },
