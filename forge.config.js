3344dbab4197c88168a5674751ad2fcb8a47c972
Andrew Chou <andrewchou@fastmail.com>
2024-06-03 18:31:47 -0400

    update forget config

--- forge.config.js
+++ forge.config.js
@@ -1,52 +1,61 @@
+// @ts-check
+
+const { MakerDeb } = require('@electron-forge/maker-deb')
+const { MakerRpm } = require('@electron-forge/maker-rpm')
+const { MakerSquirrel } = require('@electron-forge/maker-squirrel')
+const { MakerZIP } = require('@electron-forge/maker-zip')
+const { FusesPlugin } = require('@electron-forge/plugin-fuses')
+const { VitePlugin } = require('@electron-forge/plugin-vite')
+const { FuseV1Options, FuseVersion } = require('@electron/fuses')
+
+/** @type {import('@electron-forge/shared-types').ForgeConfig} */
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    asar: true,
+  },
   rebuildConfig: {},
   makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    new MakerSquirrel({}),
+    new MakerZIP({}, ['darwin']),
+    new MakerRpm({}),
+    new MakerDeb({}),
   ],
   plugins: [
-    {
-      name: '@electron-forge/plugin-vite',
-      config: {
-        // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-        // If you are familiar with Vite configuration, it will look really familiar.
-        build: [
-          {
-            // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-            entry: 'src/main/index.ts',
-            config: 'vite.main.config.mjs',
-          },
-          {
-            entry: 'src/service/mapeo-core.ts',
-            config: 'vite.service.config.mjs',
-          },
-          {
-            entry: 'src/preload/main-window.ts',
-            config: 'vite.preload.config.mjs',
-          },
-        ],
-        renderer: [
-          {
-            name: 'main_window',
-            config: 'vite.renderer.config.mjs',
-          },
-        ],
-      },
-    },
+    new VitePlugin({
+      // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
+      // If you are familiar with Vite configuration, it will look really familiar.
+      build: [
+        {
+          // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
+          entry: 'src/main/index.ts',
+          config: 'vite.main.config.mjs',
+        },
+        {
+          entry: 'src/service/mapeo-core.ts',
+          config: 'vite.service.config.mjs',
+        },
+        {
+          entry: 'src/preload/main-window.ts',
+          config: 'vite.preload.config.mjs',
+        },
+      ],
+      renderer: [
+        {
+          name: 'main_window',
+          config: 'vite.renderer.config.mjs',
+        },
+      ],
+    }),
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
   ],
 }
