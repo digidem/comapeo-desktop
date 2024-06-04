f13298574876df65508f3f195cf423785a6a6869
Andrew Chou <andrewchou@fastmail.com>
2024-06-04 17:55:28 -0400

    update electron store schema

--- src/main/store.ts
+++ src/main/store.ts
@@ -2,26 +2,9 @@ import Store from 'electron-store'
 
 import { getUserDataPath, isDevMode } from './utils'
 
-export const store = new Store<{
-  projectName: string
-  projectSuffix: string
-  configName: string
-  locale: string
-}>({
+export const store = new Store<{ locale: string }>({
   cwd: isDevMode() ? getUserDataPath() : undefined,
   schema: {
-    projectName: {
-      type: 'string',
-      default: 'Mapeo',
-    },
-    projectSuffix: {
-      type: 'string',
-      default: '',
-    },
-    configName: {
-      type: 'string',
-      default: 'mapeo-settings',
-    },
     locale: {
       type: 'string',
       default: 'en',
