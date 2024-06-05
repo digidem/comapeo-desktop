ab0e1d04db3279fdb7f935cd96fade46c9d53f5a
Andrew Chou <andrewchou@fastmail.com>
2024-06-05 10:24:54 -0400

    update product name

--- forge.config.cjs
+++ forge.config.cjs
@@ -12,6 +12,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses')
 module.exports = {
   packagerConfig: {
     asar: true,
+    name: 'CoMapeo',
   },
   rebuildConfig: {},
   makers: [
