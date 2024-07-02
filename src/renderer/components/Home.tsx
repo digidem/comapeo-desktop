f74df1ee2cd569b0e93f3942205f2e6c9390a34a
Andrew Chou <andrewchou@fastmail.com>
2024-07-02 13:57:04 -0400

    initial integration of @mapeo/core (#18)

--- src/renderer/components/Home.tsx
+++ src/renderer/components/Home.tsx
@@ -1,5 +1,6 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { defineMessages, FormattedMessage } from 'react-intl'
+
+import { useAllProjects, useCreateProject } from '../queries/projects'
 
 const m = defineMessages({
   create: {
@@ -13,49 +14,27 @@ const m = defineMessages({
 })
 
 export function Home() {
-  const queryClient = useQueryClient()
+  const allProjectsQuery = useAllProjects()
+  const createProjectMutation = useCreateProject()
 
-  const { status, data, error } = useQuery<
-    Awaited<ReturnType<typeof window.mapeo.getObservations>>,
-    Error
-  >({
-    queryKey: ['observations'],
-    queryFn: () => window.mapeo.getObservations(),
-  })
-
-  const addObservationMutation = useMutation({
-    mutationFn: (name: string) => {
-      return window.mapeo.createObservation(name)
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['observations'] })
-    },
-  })
-
-  const deleteObservationMutation = useMutation({
-    mutationFn: (name: string) => {
-      return window.mapeo.deleteObservation(name)
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['observations'] })
-    },
-  })
-
-  if (status === 'pending') return <span>Loading...</span>
-  if (status === 'error') return <span>Error: {error.message}</span>
+  if (allProjectsQuery.status === 'pending') return <span>Loading...</span>
+  if (allProjectsQuery.status === 'error')
+    return <span>Error: {allProjectsQuery.error.message}</span>
 
   return (
     <div>
-      <h1></h1>
-      <button onClick={() => addObservationMutation.mutate('andrew')}>
+      <h1>CoMapeo Desktop</h1>
+
+      <label htmlFor="create-project">Create project</label>
+      <button
+        name="create-project"
+        onClick={() => createProjectMutation.mutate(`project-${Date.now()}`)}
+      >
         <FormattedMessage {...m.create} />
       </button>
-      <button onClick={() => deleteObservationMutation.mutate('andrew')}>
-        <FormattedMessage {...m.delete} />
-      </button>
       <ul>
-        {data.map((observation, index) => (
-          <li key={`${observation}-${index}`}>{observation}</li>
+        {allProjectsQuery.data.map((project) => (
+          <li key={project.projectId}>{project.name || 'No name'}</li>
         ))}
       </ul>
     </div>
