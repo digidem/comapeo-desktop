08ce9d82f64f5a1eb19f8222a3123c25c31fc1b7
Andrew Chou <andrewchou@fastmail.com>
2024-10-29 15:59:26 -0400

    remove comapeo window global

--- src/renderer/global.d.ts
+++ src/renderer/global.d.ts
@@ -1,11 +1,8 @@
-import { type MapeoClientApi } from '@comapeo/ipc'
-
 import { type RuntimeApi } from '../preload/runtime'
 
 declare global {
 	// Make changes here whenever you expose new things in the preload/ using exposeInMainWorld
 	interface Window {
 		runtime: RuntimeApi
-		comapeo: MapeoClientApi
 	}
 }
