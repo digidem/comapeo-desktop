99270f3a463b520033600610f9a0f2a9841a9cb8
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-11-21 16:48:19 +0000

    choreremove comments

--- src/renderer/src/index.tsx
+++ src/renderer/src/index.tsx
@@ -1,15 +1,12 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { createRoot } from 'react-dom/client'
 
-// Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
 import './index.css'
 
-// Create a new router instance
 const router = createRouter({ routeTree })
 
-// Register the router instance for type safety
 declare module '@tanstack/react-router' {
 	interface Register {
 		router: typeof router
