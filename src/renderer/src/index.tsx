114e197e07bde38e4ab94ae29d3c1e3d69a433c8
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-11-21 16:47:51 +0000

    chore: created router configuration in jsx files

--- src/renderer/src/index.tsx
+++ src/renderer/src/index.tsx
@@ -1,9 +1,21 @@
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { createRoot } from 'react-dom/client'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
 
 import './index.css'
 
-import { App } from './App'
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router
+	}
+}
 
 const root = createRoot(document.getElementById('app') as HTMLElement)
 
-root.render(<App />)
+root.render(<RouterProvider router={router} />)
