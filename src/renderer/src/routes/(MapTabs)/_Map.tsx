36d5b15bca32e3097938dea0ffa7550db039da87
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-12-10 14:49:18 -0800

    chore: update Map Layout

--- src/renderer/src/routes/(MapTabs)/_Map.tsx
+++ src/renderer/src/routes/(MapTabs)/_Map.tsx
@@ -1,10 +1,38 @@
-import * as React from 'react'
-import { Paper } from '@mui/material'
+import { Suspense } from 'react'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
+import PostAddIcon from '@mui/icons-material/PostAdd'
+import SettingsIcon from '@mui/icons-material/Settings'
+import { Box, CircularProgress, Paper } from '@mui/material'
 import Tab from '@mui/material/Tab'
 import Tabs from '@mui/material/Tabs'
+import { styled } from '@mui/material/styles'
 import { Outlet, createFileRoute, useNavigate } from '@tanstack/react-router'
+import { defineMessages, useIntl } from 'react-intl'
 
+import { VERY_LIGHT_GREY, WHITE } from '../../colors'
+import { Text } from '../../components/Text'
 import type { FileRoutesById } from '../../routeTree.gen'
+
+const m = defineMessages({
+	setting: {
+		id: 'tabBar.label.settings',
+		defaultMessage: 'Settings',
+	},
+	about: {
+		id: 'tabBar.label.about',
+		defaultMessage: 'About',
+	},
+})
+
+const Container = styled('div')({
+	display: 'flex',
+	backgroundColor: WHITE,
+	height: '100%',
+})
+
+const MapTabStyled = styled(MapTab)({
+	width: 60,
+})
 
 export const Route = createFileRoute('/(MapTabs)/_Map')({
 	component: RouteComponent,
@@ -12,23 +40,56 @@ export const Route = createFileRoute('/(MapTabs)/_Map')({
 
 function RouteComponent() {
 	const navigate = useNavigate()
-	const renderCount = React.useRef(0)
-	renderCount.current = renderCount.current + 1
+	const { formatMessage } = useIntl()
 	return (
-		<div>
-			<Tabs
-				onChange={(_, value) => navigate({ to: value as MapTabRoute })}
-				orientation="vertical"
-			>
-				<MapTab label="Tab 1" value={'/tab1'} />
-				<MapTab label="Tab 2" value={'/tab2'} />
-			</Tabs>
-			<Paper>
-				<Outlet />
+		<Container>
+			<Paper elevation={3} sx={{ display: 'flex' }}>
+				<Tabs
+					sx={{ '& .MuiTabs-flexContainer': { height: '100%' } }}
+					onChange={(_, value) => navigate({ to: value as MapTabRoute })}
+					orientation="vertical"
+				>
+					{/* PostAddIcon is a placeholder icon */}
+					<MapTabStyled icon={<PostAddIcon />} value={'/tab1'} />
+					<Box flexGrow={1} />
+
+					<MapTabStyled
+						icon={<SettingsIcon />}
+						label={
+							<Text style={{ fontSize: 10 }} kind="title">
+								{formatMessage(m.setting)}
+							</Text>
+						}
+						value={'/tab2'}
+					/>
+					<MapTabStyled
+						icon={<InfoOutlinedIcon />}
+						label={
+							<Text style={{ fontSize: 10 }} kind="title">
+								{formatMessage(m.about)}
+							</Text>
+						}
+						value={'/tab2'}
+					/>
+				</Tabs>
+
+				<Box
+					sx={{
+						width: 300,
+						borderLeftColor: VERY_LIGHT_GREY,
+						borderLeftWidth: '1px',
+						borderLeftStyle: 'solid',
+					}}
+				>
+					<Suspense fallback={<CircularProgress />}>
+						<Outlet />
+					</Suspense>
+				</Box>
 			</Paper>
-			<div>map component here</div>
-			<div>parent map component render count: {renderCount.current}</div>
-		</div>
+			<Suspense fallback={<CircularProgress />}>
+				<div>map component here</div>
+			</Suspense>
+		</Container>
 	)
 }
 
