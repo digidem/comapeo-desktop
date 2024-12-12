e330b4332bbb1356c870bd34a372429f808d1947
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-12-12 12:49:01 -0800

    chore: update index route to redirect in useLayoutEffect

--- src/renderer/src/routes/index.tsx
+++ src/renderer/src/routes/index.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute, useRouter } from '@tanstack/react-router'
+import { useLayoutEffect } from 'react'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
 
 import { useDeviceInfo } from '../queries/deviceInfo'
 
@@ -6,14 +7,21 @@ export const Route = createFileRoute('/')({
 	component: RouteComponent,
 })
 
+// the user will never get here, they will be redirected.
+// While this code is semi hacky, the suggested alternative is to redirect in the (beforeLoad)[https://tanstack.com/router/latest/docs/framework/react/guide/authenticated-routes#the-routebeforeload-option]. The problem is that this requires the router to use 'useDeviceInfo'. We could pass this hook to the router via the RouterContext. But i think the complexity of passing that info makes this hacky code slightly more desirable and easy to understand.
+
 function RouteComponent() {
-	const router = useRouter()
+	const navigate = useNavigate()
 	const { data } = useDeviceInfo()
 	const hasCreatedDeviceName = data?.name !== undefined
 
-	if (!hasCreatedDeviceName) {
-		router.navigate({ to: '/Onboarding' })
-	} else {
-		router.navigate({ to: '/tab1' })
-	}
+	useLayoutEffect(() => {
+		if (!hasCreatedDeviceName) {
+			navigate({ to: '/Onboarding' })
+		} else {
+			navigate({ to: '/tab1' })
+		}
+	})
+
+	return null
 }
