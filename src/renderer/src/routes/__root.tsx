68636fa6993c375b2a2d72c698459b8468eb2a5c
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-12-12 13:21:34 -0800

    chore: remove import

--- src/renderer/src/routes/__root.tsx
+++ src/renderer/src/routes/__root.tsx
@@ -3,7 +3,6 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 import CircularProgress from '@mui/material/CircularProgress'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 import { theme } from '../Theme'
 import { ApiProvider } from '../contexts/ApiContext'
