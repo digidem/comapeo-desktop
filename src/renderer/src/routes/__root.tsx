1dd0cb3600839823946dea8c9a2eada2548eb2f3
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-12-14 18:31:28 -0800

    chore: update root and index route

--- src/renderer/src/routes/__root.tsx
+++ src/renderer/src/routes/__root.tsx
@@ -1,28 +1,16 @@
 import { Suspense } from 'react'
-import { CssBaseline, ThemeProvider } from '@mui/material'
 import CircularProgress from '@mui/material/CircularProgress'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 
-import { theme } from '../Theme'
-import { ApiProvider } from '../contexts/ApiContext'
-import { IntlProvider } from '../contexts/IntlContext'
+type RouterContext = {
+	hasDeviceName: boolean
+	persistedProjectId: boolean
+}
 
-const queryClient = new QueryClient()
-
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<RouterContext>()({
 	component: () => (
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<IntlProvider>
-				<QueryClientProvider client={queryClient}>
-					<ApiProvider>
-						<Suspense fallback={<CircularProgress />}>
-							<Outlet />
-						</Suspense>
-					</ApiProvider>
-				</QueryClientProvider>
-			</IntlProvider>
-		</ThemeProvider>
+		<Suspense fallback={<CircularProgress />}>
+			<Outlet />
+		</Suspense>
 	),
 })
--- src/renderer/src/routes/index.tsx
+++ src/renderer/src/routes/index.tsx
@@ -1,27 +1,13 @@
-import { useLayoutEffect } from 'react'
-import { createFileRoute, useNavigate } from '@tanstack/react-router'
-
-import { useDeviceInfo } from '../queries/deviceInfo'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-	component: RouteComponent,
-})
-
-// the user will never get here, they will be redirected.
-// While this code is semi hacky, the suggested alternative is to redirect in the (beforeLoad)[https://tanstack.com/router/latest/docs/framework/react/guide/authenticated-routes#the-routebeforeload-option]. The problem is that this requires the router to use 'useDeviceInfo'. We could pass this hook to the router via the RouterContext. But i think the complexity of passing that info makes this hacky code slightly more desirable and easy to understand.
-
-function RouteComponent() {
-	const navigate = useNavigate()
-	const { data } = useDeviceInfo()
-	const hasCreatedDeviceName = data?.name !== undefined
-
-	useLayoutEffect(() => {
-		if (!hasCreatedDeviceName) {
-			navigate({ to: '/Onboarding' })
-		} else {
-			navigate({ to: '/tab1' })
+	beforeLoad: ({ context }) => {
+		if (!context.hasDeviceName) {
+			throw redirect({ to: '/Onboarding' })
 		}
-	})
-
-	return null
-}
+		if (!context.persistedProjectId) {
+			throw redirect({ to: '/Onboarding/CreateJoinProjectScreen' })
+		}
+		throw redirect({ to: '/tab1' })
+	},
+})
