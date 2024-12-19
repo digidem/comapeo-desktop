56831f39f38e7f799d242471643a00c156e7b9fd
ErikSin <67773827+ErikSin@users.noreply.github.com>
2024-12-18 21:05:02 -0800

    chore: create guaranteed projectIdProvider

--- src/renderer/src/routes/_Map.tsx
+++ src/renderer/src/routes/_Map.tsx
@@ -1,9 +1,11 @@
 import { Paper } from '@mui/material'
 import { styled } from '@mui/material/styles'
-import { Outlet, createFileRoute } from '@tanstack/react-router'
+import { Outlet, createFileRoute, redirect } from '@tanstack/react-router'
 
+import { PersistedProjectIdStore } from '../App'
 import { VERY_LIGHT_GREY, WHITE } from '../colors'
 import { Tabs } from '../components/Tabs'
+import { GuaranteedActiveProjectIdProvider } from '../contexts/GuaranteedActiveProjectIdProvider'
 
 const Container = styled('div')({
 	display: 'flex',
@@ -13,25 +15,34 @@ const Container = styled('div')({
 
 export const Route = createFileRoute('/_Map')({
 	component: MapLayout,
+	beforeLoad: () => {
+		const activeProjectId =
+			PersistedProjectIdStore.store.getState().activeProjectId
+		if (!activeProjectId) {
+			throw redirect({ to: '/' })
+		}
+	},
 })
 
 export function MapLayout() {
 	return (
-		<Container>
-			<Paper elevation={3} sx={{ display: 'flex' }}>
-				<Tabs />
-				<div
-					style={{
-						width: 300,
-						borderLeftColor: VERY_LIGHT_GREY,
-						borderLeftWidth: '1px',
-						borderLeftStyle: 'solid',
-					}}
-				>
-					<Outlet />
-				</div>
-			</Paper>
-			<div>map component here</div>
-		</Container>
+		<GuaranteedActiveProjectIdProvider>
+			<Container>
+				<Paper elevation={3} sx={{ display: 'flex' }}>
+					<Tabs />
+					<div
+						style={{
+							width: 300,
+							borderLeftColor: VERY_LIGHT_GREY,
+							borderLeftWidth: '1px',
+							borderLeftStyle: 'solid',
+						}}
+					>
+						<Outlet />
+					</div>
+				</Paper>
+				<div>map component here</div>
+			</Container>
+		</GuaranteedActiveProjectIdProvider>
 	)
 }
--- src/renderer/src/routes/_Map/Tab1.tsx
+++ src/renderer/src/routes/_Map/Tab1.tsx
@@ -2,15 +2,18 @@ import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
 
 import { Text } from '../../components/Text'
+import { useGuaranteedActiveProjectId } from '../../contexts/GuaranteedActiveProjectIdProvider'
 
 export const Route = createFileRoute('/_Map/Tab1')({
 	component: Observations,
 })
 
 export function Observations() {
+	const projectId = useGuaranteedActiveProjectId()
 	return (
 		<div>
 			<Text>Tab 1</Text>
+			<Text>{projectId}</Text>
 		</div>
 	)
 }
