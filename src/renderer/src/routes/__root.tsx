8dfcec7758085f7d9ddb3a65ff277e33d591f760
Andrew Chou <andrewchou@fastmail.com>
2024-12-19 15:57:14 -0500

    fix merge issues

--- src/renderer/src/routes/__root.tsx
+++ src/renderer/src/routes/__root.tsx
@@ -1,30 +1,11 @@
 import { Suspense } from 'react'
-import { ClientApiProvider } from '@comapeo/core-react'
-import { CssBaseline, ThemeProvider } from '@mui/material'
 import CircularProgress from '@mui/material/CircularProgress'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-
-import { theme } from '../Theme'
-import { initComapeoClient } from '../comapeo-client.js'
-import { IntlProvider } from '../contexts/IntlContext'
-
-const queryClient = new QueryClient()
-const comapeoClient = initComapeoClient()
 
 export const Route = createRootRoute({
 	component: () => (
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<IntlProvider>
-				<QueryClientProvider client={queryClient}>
-					<ClientApiProvider clientApi={comapeoClient}>
-						<Suspense fallback={<CircularProgress />}>
-							<Outlet />
-						</Suspense>
-					</ClientApiProvider>
-				</QueryClientProvider>
-			</IntlProvider>
-		</ThemeProvider>
+		<Suspense fallback={<CircularProgress />}>
+			<Outlet />
+		</Suspense>
 	),
 })
