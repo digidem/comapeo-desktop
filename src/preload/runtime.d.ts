80b5fb5be6f1a91fd5840eba53b66c3e2df47644
Andrew Chou <andrewchou@fastmail.com>
2025-01-07 17:03:11 -0500

    feat: update runtime.selectFile() to also return the file name (#81)

--- src/preload/runtime.d.ts
+++ src/preload/runtime.d.ts
@@ -1,5 +1,12 @@
+export type SelectedFile = {
+	name: string
+	path: string
+}
+
 export type RuntimeApi = {
 	getLocale: () => Promise<string>
 	updateLocale: (locale: string) => void
-	selectFile: (extensionFilters?: Array<string>) => Promise<string | undefined>
+	selectFile: (
+		extensionFilters?: Array<string>,
+	) => Promise<SelectedFile | undefined>
 }
