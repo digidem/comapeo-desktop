7380da96dc50a4b9fcfad14bf5a2e152fbc7bc28
cimigree <cgreen@digital-democracy.org>
2025-01-07 15:11:31 -0500

    chore: integrate map libre (#61)
    * Adds basic map libre map.

---
+++ src/renderer/src/components/Map.tsx
@@ -0,0 +1,28 @@
+import { Map as ReactMapLibre } from '@vis.gl/react-maplibre'
+
+import 'maplibre-gl/dist/maplibre-gl.css'
+
+export function Map() {
+	const center = [-72.312023, -10.38787]
+
+	return (
+		<ReactMapLibre
+			initialViewState={{
+				longitude: center[0],
+				latitude: center[1],
+				zoom: 6,
+			}}
+			dragPan={true}
+			scrollZoom={true}
+			doubleClickZoom={true}
+			style={{ width: '100%', height: '100%' }}
+			mapStyle="https://demotiles.maplibre.org/style.json"
+			onError={(evt) => {
+				console.error('Map error:', evt.error)
+			}}
+			onLoad={() => {
+				console.log('Map loaded successfully!')
+			}}
+		/>
+	)
+}
