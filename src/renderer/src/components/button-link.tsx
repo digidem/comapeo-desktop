16bdb623a911aa7c1b7fc6d347b26033c78c4664
Andrew Chou <andrewchou@fastmail.com>
2025-07-14 12:03:05 -0400

    fix: disable navigation tabs when on some page with editable

--- src/renderer/src/components/button-link.tsx
+++ src/renderer/src/components/button-link.tsx
@@ -2,14 +2,35 @@
  * Based on
  * https://tanstack.com/router/latest/docs/framework/react/guide/custom-link#button
  */
+import type { ComponentProps } from 'react'
 import MUIButton, { type ButtonProps } from '@mui/material/Button'
 import MUIIconButton, { type IconButtonProps } from '@mui/material/IconButton'
-import { createLink } from '@tanstack/react-router'
+import { createLink, type LinkOptions } from '@tanstack/react-router'
 
-export const ButtonLink = createLink((props: ButtonProps<'a'>) => {
+const ButtonLinkComponent = createLink((props: ButtonProps<'a'>) => {
 	return <MUIButton component="a" ref={props.ref} {...props} />
 })
 
-export const IconButtonLink = createLink((props: IconButtonProps<'a'>) => {
+export type ButtonLinkProps = Omit<
+	ComponentProps<typeof ButtonLinkComponent>,
+	keyof LinkOptions
+> &
+	LinkOptions
+
+export function ButtonLink(props: ButtonLinkProps) {
+	return <ButtonLinkComponent {...props} />
+}
+
+const IconButtonLinkComponent = createLink((props: IconButtonProps<'a'>) => {
 	return <MUIIconButton component="a" ref={props.ref} {...props} />
 })
+
+export type IconButtonLinkProps = Omit<
+	ComponentProps<typeof IconButtonLinkComponent>,
+	keyof LinkOptions
+> &
+	LinkOptions
+
+export function IconButtonLink(props: IconButtonLinkProps) {
+	return <IconButtonLinkComponent {...props} />
+}
