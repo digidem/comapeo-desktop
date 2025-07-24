80987796abc7df14be20a3912e65bcebebcaa662
Andrew Chou <andrewchou@fastmail.com>
2025-07-24 14:01:04 -0400

    disable map interactions when viewing page of specific feature

--- src/renderer/src/components/map.tsx
+++ src/renderer/src/components/map.tsx
@@ -1,3 +1,4 @@
+import type { Ref } from 'react'
 import {
 	Map as ReactMapLibre,
 	type MapProps,
@@ -6,23 +7,39 @@ import {
 
 import 'maplibre-gl/dist/maplibre-gl.css'
 
-import type { Ref } from 'react'
-
 // TODO: Temporary. Need to fix https://github.com/digidem/comapeo-desktop/issues/98
 const FALLBACK_MAP_STYLE = 'https://demotiles.maplibre.org/style.json'
 
 export function Map({
-	ref,
+	doubleClickZoom,
+	dragPan,
+	dragRotate,
+	interactive,
 	mapStyle = FALLBACK_MAP_STYLE,
+	ref,
 	reuseMaps = true,
+	scrollZoom,
+	touchPitch,
+	touchZoomRotate,
 	...rest
 }: MapProps & { ref?: Ref<MapRef> }) {
 	return (
 		<ReactMapLibre
-			ref={ref}
 			{...rest}
-			reuseMaps={reuseMaps}
+			ref={ref}
+			dragPan={interactive === undefined ? dragPan : interactive}
+			dragRotate={interactive === undefined ? dragRotate : interactive}
+			doubleClickZoom={
+				interactive === undefined ? doubleClickZoom : interactive
+			}
+			interactive={interactive}
 			mapStyle={mapStyle}
+			reuseMaps={reuseMaps}
+			scrollZoom={interactive === undefined ? scrollZoom : interactive}
+			touchPitch={interactive === undefined ? touchPitch : interactive}
+			touchZoomRotate={
+				interactive === undefined ? touchZoomRotate : interactive
+			}
 		/>
 	)
 }
