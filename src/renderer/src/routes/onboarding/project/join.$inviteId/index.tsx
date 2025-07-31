57099657aa82b18f7a2c5aad5ec0450f264fc737
Andrew Chou <andrewchou@fastmail.com>
2025-07-31 15:50:29 -0400

    use error dialog in project join onboarding page

--- src/renderer/src/routes/onboarding/project/join.$inviteId/index.tsx
+++ src/renderer/src/routes/onboarding/project/join.$inviteId/index.tsx
@@ -21,6 +21,7 @@ import {
 	RED,
 	WHITE,
 } from '../../../../colors'
+import { ErrorDialog } from '../../../../components/error-dialog'
 import { Icon } from '../../../../components/icon'
 import { COMAPEO_CORE_REACT_ROOT_QUERY_KEY } from '../../../../lib/comapeo'
 import { setActiveProjectIdMutationOptions } from '../../../../lib/queries/app-settings'
@@ -59,148 +60,174 @@ function RouteComponent() {
 		setActiveProjectIdMutationOptions(queryClient),
 	)
 
+	const errorDialogProps =
+		rejectInvite.status === 'error'
+			? {
+					open: true,
+					errorMessage: rejectInvite.error.message,
+					onClose: () => {
+						rejectInvite.reset()
+					},
+				}
+			: acceptInvite.status === 'error'
+				? {
+						open: true,
+						errorMessage: acceptInvite.error.message,
+						onClose: () => {
+							acceptInvite.reset()
+						},
+					}
+				: { open: false, onClose: () => {} }
+
 	return (
-		<Stack
-			display="flex"
-			useFlexGap
-			direction="column"
-			justifyContent="space-between"
-			flex={1}
-			gap={10}
-			bgcolor={LIGHT_GREY}
-			padding={5}
-			borderRadius={2}
-			overflow="auto"
-		>
-			<Container
-				maxWidth="sm"
-				component={Stack}
-				direction="column"
+		<>
+			<Stack
+				display="flex"
 				useFlexGap
-				gap={5}
+				direction="column"
+				justifyContent="space-between"
+				flex={1}
+				gap={10}
+				bgcolor={LIGHT_GREY}
+				padding={5}
+				borderRadius={2}
+				overflow="auto"
 			>
-				<Box alignSelf="center">
-					<Icon
-						name="material-person-add"
-						size={80}
-						htmlColor={DARKER_ORANGE}
-					/>
-				</Box>
-
-				<Typography variant="h1" fontWeight={500} textAlign="center">
-					{t(m.title)}
-				</Typography>
-
-				<Typography variant="h2" fontWeight={400} textAlign="center">
-					{t(m.description)}
-				</Typography>
-
-				<Stack
+				<Container
+					maxWidth="sm"
+					component={Stack}
 					direction="column"
 					useFlexGap
-					gap={4}
-					borderRadius={2}
-					padding={6}
-					boxShadow={BOX_SHADOW}
-					bgcolor={invite.projectColor || WHITE}
-					border={`2px solid ${acceptInvite.status === 'error' || rejectInvite.status === 'error' ? RED : LIGHT_GREY}`}
+					gap={5}
 				>
-					<Typography variant="h1" fontWeight={500}>
-						{invite.projectName}
+					<Box alignSelf="center">
+						<Icon
+							name="material-person-add"
+							size={80}
+							htmlColor={DARKER_ORANGE}
+						/>
+					</Box>
+
+					<Typography variant="h1" fontWeight={500} textAlign="center">
+						{t(m.title)}
 					</Typography>
 
-					<Typography variant="h2" fontWeight={400} sx={{ color: DARK_GREY }}>
-						{t(m.joinAs, { role: invite.roleName?.toLowerCase() })}
+					<Typography variant="h2" fontWeight={400} textAlign="center">
+						{t(m.description)}
 					</Typography>
-				</Stack>
 
-				{(acceptInvite.status === 'error' ||
-					rejectInvite.status === 'error') && (
-					<Typography color="error">{t(m.responseError)}</Typography>
-				)}
-			</Container>
+					<Stack
+						direction="column"
+						useFlexGap
+						gap={4}
+						borderRadius={2}
+						padding={6}
+						boxShadow={BOX_SHADOW}
+						bgcolor={invite.projectColor || WHITE}
+						border={`2px solid ${acceptInvite.status === 'error' || rejectInvite.status === 'error' ? RED : LIGHT_GREY}`}
+					>
+						<Typography variant="h1" fontWeight={500}>
+							{invite.projectName}
+						</Typography>
 
-			<Container
-				component={Stack}
-				direction="row"
-				justifyContent="center"
-				useFlexGap
-				gap={5}
-			>
-				<Button
-					variant="outlined"
-					size="large"
-					disableElevation
-					fullWidth
-					sx={{ maxWidth: 400 }}
-					// TODO: Consider using spin-delay
-					loading={rejectInvite.status === 'pending'}
-					loadingPosition="start"
-					onClick={() => {
-						if (acceptInvite.status === 'pending') {
-							return
-						}
+						<Typography variant="h2" fontWeight={400} sx={{ color: DARK_GREY }}>
+							{t(m.joinAs, { role: invite.roleName?.toLowerCase() })}
+						</Typography>
+					</Stack>
 
-						acceptInvite.reset()
-						rejectInvite.reset()
+					{(acceptInvite.status === 'error' ||
+						rejectInvite.status === 'error') && (
+						<Typography color="error">{t(m.responseError)}</Typography>
+					)}
+				</Container>
 
-						rejectInvite.mutate(
-							{ inviteId },
-							{
-								onError: (_err) => {
-									// TODO: Sentry
-								},
-								onSuccess: () => {
-									if (router.history.canGoBack()) {
-										router.history.back()
-										return
-									}
-
-									navigate({ to: '/onboarding/project', replace: true })
-								},
-							},
-						)
-					}}
+				<Container
+					component={Stack}
+					direction="row"
+					justifyContent="center"
+					useFlexGap
+					gap={5}
 				>
-					{t(m.declineInvite)}
-				</Button>
+					<Button
+						variant="outlined"
+						size="large"
+						disableElevation
+						fullWidth
+						sx={{ maxWidth: 400 }}
+						// TODO: Consider using spin-delay
+						loading={rejectInvite.status === 'pending'}
+						loadingPosition="start"
+						onClick={() => {
+							if (acceptInvite.status === 'pending') {
+								return
+							}
 
-				<Button
-					variant="contained"
-					size="large"
-					disableElevation
-					fullWidth
-					sx={{ maxWidth: 400 }}
-					// TODO: Consider using spin-delay
-					loading={acceptInvite.status === 'pending'}
-					loadingPosition="start"
-					onClick={() => {
-						if (rejectInvite.status === 'pending') {
-							return
-						}
+							acceptInvite.reset()
+							rejectInvite.reset()
 
-						acceptInvite.reset()
-						rejectInvite.reset()
+							rejectInvite.mutate(
+								{ inviteId },
+								{
+									onError: (_err) => {
+										// TODO: Sentry
+									},
+									onSuccess: () => {
+										if (router.history.canGoBack()) {
+											router.history.back()
+											return
+										}
 
-						acceptInvite.mutate(
-							{ inviteId },
-							{
-								onSuccess: (data) => {
-									setActiveProjectId.mutate(data)
-
-									navigate({
-										to: '/onboarding/project/join/$inviteId/success',
-										params: { inviteId },
-									})
+										navigate({ to: '/onboarding/project', replace: true })
+									},
 								},
-							},
-						)
-					}}
-				>
-					{t(m.joinProject)}
-				</Button>
-			</Container>
-		</Stack>
+							)
+						}}
+					>
+						{t(m.declineInvite)}
+					</Button>
+
+					<Button
+						variant="contained"
+						size="large"
+						disableElevation
+						fullWidth
+						sx={{ maxWidth: 400 }}
+						// TODO: Consider using spin-delay
+						loading={acceptInvite.status === 'pending'}
+						loadingPosition="start"
+						onClick={() => {
+							if (rejectInvite.status === 'pending') {
+								return
+							}
+
+							acceptInvite.reset()
+							rejectInvite.reset()
+
+							acceptInvite.mutate(
+								{ inviteId },
+								{
+									onError: (_err) => {
+										// TODO: Report to Sentry
+									},
+									onSuccess: (data) => {
+										setActiveProjectId.mutate(data)
+
+										navigate({
+											to: '/onboarding/project/join/$inviteId/success',
+											params: { inviteId },
+										})
+									},
+								},
+							)
+						}}
+					>
+						{t(m.joinProject)}
+					</Button>
+				</Container>
+			</Stack>
+
+			<ErrorDialog {...errorDialogProps} />
+		</>
 	)
 }
 
