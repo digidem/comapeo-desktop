digidem/comapeo-desktop

commit b51dcd0b29c2f61a393c2c3b84ca1fc9fab7b64d

chore: create reusable component for category icon image (#230)
The repetition was getting a bit unwieldy and I finally caved into
creating a component for this.

src/renderer/src/components/category-icon.tsx
@@ -1,8 +1,18 @@
-import type { ComponentProps, ReactNode } from 'react'
+import {
+	Suspense,
+	type CSSProperties,
+	type ComponentProps,
+	type ReactNode,
+} from 'react'
+import { useIconUrl } from '@comapeo/core-react'
 import Box from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress'
 import { alpha } from '@mui/material/styles'
 
 import { WHITE } from '../colors'
+import { ErrorBoundary } from './error-boundary'
+import { Icon } from './icon'
+import { SuspenseImage } from './suspense-image'
 
 export function CategoryIconContainer({
 	children,
@@ -32,5 +42,36 @@ export function CategoryIconContainer({
 		>
 			{children}
 		</Box>
+	)
+}
+
+export function CategoryIconImage({
+	altText,
+	projectId,
+	iconDocumentId,
+	imageStyle,
+}: {
+	altText: string
+	iconDocumentId: string
+	projectId: string
+	imageStyle?: CSSProperties
+}) {
+	const { data: iconUrl } = useIconUrl({
+		projectId,
+		iconId: iconDocumentId,
+		mimeType: 'image/png',
+		size: 'small',
+		pixelDensity: 3,
+	})
+
+	return (
+		<ErrorBoundary
+			getResetKey={() => iconUrl}
+			fallback={() => <Icon name="material-error" color="error" />}
+		>
+			<Suspense fallback={<CircularProgress disableShrink />}>
+				<SuspenseImage src={iconUrl} alt={altText} style={imageStyle} />
+			</Suspense>
+		</ErrorBoundary>
 	)
 }

src/renderer/src/routes/app/projects/$projectId/-displayed-data/list.tsx
@@ -4,7 +4,6 @@ import {
 	useEffect,
 	useMemo,
 	useRef,
-	type CSSProperties,
 	type FocusEvent,
 	type MouseEvent,
 	type RefObject,
@@ -13,7 +12,6 @@ import type { BlobApi } from '@comapeo/core'
 import {
 	useAttachmentUrl,
 	useDocumentCreatedBy,
-	useIconUrl,
 	useManyDocs,
 	useOwnDeviceInfo,
 } from '@comapeo/core-react'
@@ -38,7 +36,10 @@ import {
 	LIGHT_GREY,
 	WHITE,
 } from '../../../../../colors'
-import { CategoryIconContainer } from '../../../../../components/category-icon'
+import {
+	CategoryIconContainer,
+	CategoryIconImage,
+} from '../../../../../components/category-icon'
 import { ErrorBoundary } from '../../../../../components/error-boundary'
 import { Icon } from '../../../../../components/icon'
 import { TextLink } from '../../../../../components/link'
@@ -238,7 +239,6 @@ export function DisplayedDataList({ projectId }: { projectId: string }) {
 							}}
 						>
 							<ListItemButton
-								key={docId}
 								data-datatype={type}
 								data-docid={docId}
 								disableGutters
@@ -429,27 +429,26 @@ function ObservationCategory({
 	if (displayableAttachments.length > 0) {
 		const shouldStack = displayableAttachments.length > 1
 
-		const categoryIcon = categoryIconDocumentId ? (
-			<CategoryIcon
-				projectId={projectId}
-				iconDocumentId={categoryIconDocumentId}
-				categoryColor={color}
-				categoryName={name}
-				imageStyle={{ aspectRatio: 1, maxHeight: 12, objectFit: 'cover' }}
-			/>
-		) : (
-			<CategoryIconContainer color={BLUE_GREY}>
-				<Box
-					display="flex"
-					justifyContent="center"
-					alignItems="center"
-					maxHeight={12}
-					sx={{
-						aspectRatio: 1,
-					}}
-				>
-					<Icon name="material-place" />
-				</Box>
+		const categoryIcon = (
+			<CategoryIconContainer color={color}>
+				{categoryIconDocumentId ? (
+					<CategoryIconImage
+						projectId={projectId}
+						iconDocumentId={categoryIconDocumentId}
+						altText={t(m.categoryIconAlt, { name })}
+						imageStyle={{ aspectRatio: 1, maxHeight: 12, objectFit: 'cover' }}
+					/>
+				) : (
+					<Box
+						display="flex"
+						justifyContent="center"
+						alignItems="center"
+						maxHeight={12}
+						sx={{ aspectRatio: 1 }}
+					>
+						<Icon name="material-place" />
+					</Box>
+				)}
 			</CategoryIconContainer>
 		)
 
@@ -536,13 +535,14 @@ function ObservationCategory({
 	}
 
 	return categoryIconDocumentId ? (
-		<CategoryIcon
-			projectId={projectId}
-			iconDocumentId={categoryIconDocumentId}
-			categoryColor={color}
-			categoryName={name}
-			imageStyle={{ aspectRatio: 1, width: '100%' }}
-		/>
+		<CategoryIconContainer color={color}>
+			<CategoryIconImage
+				projectId={projectId}
+				iconDocumentId={categoryIconDocumentId}
+				altText={t(m.categoryIconAlt, { name })}
+				imageStyle={{ aspectRatio: 1, width: '100%' }}
+			/>
+		</CategoryIconContainer>
 	) : (
 		<CategoryIconContainer color={BLUE_GREY}>
 			<Icon name="material-place" size={40} />
@@ -564,65 +564,19 @@ function TrackCategory({
 	const { formatMessage: t } = useIntl()
 
 	return categoryIconDocumentId ? (
-		<CategoryIcon
-			projectId={projectId}
-			iconDocumentId={categoryIconDocumentId}
-			categoryColor={categoryColor || BLUE_GREY}
-			categoryName={categoryName || t(m.trackItemTitle)}
-			imageStyle={{ aspectRatio: 1, width: '100%' }}
-		/>
+		<CategoryIconContainer color={categoryColor || BLUE_GREY}>
+			<CategoryIconImage
+				projectId={projectId}
+				iconDocumentId={categoryIconDocumentId}
+				altText={t(m.categoryIconAlt, {
+					name: categoryName || t(m.trackItemTitle),
+				})}
+				imageStyle={{ aspectRatio: 1, width: '100%' }}
+			/>
+		</CategoryIconContainer>
 	) : (
 		<CategoryIconContainer color={BLACK}>
 			<Icon name="material-hiking" size={40} />
-		</CategoryIconContainer>
-	)
-}
-
-function CategoryIcon({
-	categoryColor,
-	categoryName,
-	iconDocumentId,
-	imageStyle,
-	projectId,
-}: {
-	categoryColor: string
-	categoryName: string
-	iconDocumentId: string
-	imageStyle?: CSSProperties
-	projectId: string
-}) {
-	const { formatMessage: t } = useIntl()
-
-	const { data: iconUrl } = useIconUrl({
-		projectId,
-		iconId: iconDocumentId,
-		mimeType: 'image/png',
-		size: 'small',
-		pixelDensity: 3,
-	})
-
-	return (
-		<CategoryIconContainer color={categoryColor}>
-			<ErrorBoundary
-				getResetKey={() => iconUrl}
-				fallback={() => (
-					<Box
-						sx={imageStyle}
-						display="flex"
-						justifyContent="center"
-						alignItems="center"
-						flex={1}
-					>
-						<Icon name="material-error" color="error" />
-					</Box>
-				)}
-			>
-				<SuspenseImage
-					src={iconUrl}
-					alt={t(m.categoryIconAlt, { name: categoryName })}
-					style={imageStyle}
-				/>
-			</ErrorBoundary>
 		</CategoryIconContainer>
 	)
 }

src/renderer/src/routes/app/projects/$projectId/-displayed-data/map.tsx
@@ -7,7 +7,6 @@ import {
 	useState,
 } from 'react'
 import {
-	useIconUrl,
 	useManyDocs,
 	useMapStyleUrl,
 	useSingleDocByDocId,
@@ -39,7 +38,10 @@ import {
 import * as v from 'valibot'
 
 import { BLACK, BLUE_GREY, ORANGE, WHITE } from '../../../../../colors'
-import { CategoryIconContainer } from '../../../../../components/category-icon'
+import {
+	CategoryIconContainer,
+	CategoryIconImage,
+} from '../../../../../components/category-icon'
 import { Icon } from '../../../../../components/icon'
 import { Map } from '../../../../../components/map'
 import { getMatchingCategoryForDocument } from '../../../../../lib/comapeo'
@@ -507,6 +509,8 @@ export function DisplayedDataMap() {
 	)
 }
 
+const ICON_SIZE = 20
+
 function CategoryIconMarker({
 	projectId,
 	documentId,
@@ -528,29 +532,26 @@ function CategoryIconMarker({
 	const categoryDocIdToUse = categoryDocumentId || document.presetRef?.docId
 
 	return categoryDocIdToUse ? (
-		<CategoryIconImage
+		<CategoryIcon
 			categoryDocumentId={categoryDocIdToUse}
 			projectId={projectId}
-			size={20}
 			lang={lang}
 		/>
 	) : (
 		<CategoryIconContainer color={BLUE_GREY} applyBoxShadow padding={1}>
-			<Icon name="material-place" size={20} />
+			<Icon name="material-place" size={ICON_SIZE} />
 		</CategoryIconContainer>
 	)
 }
 
-function CategoryIconImage({
+function CategoryIcon({
 	categoryDocumentId,
 	lang,
 	projectId,
-	size,
 }: {
 	categoryDocumentId: string
 	lang: string
 	projectId: string
-	size: number
 }) {
 	const { formatMessage: t } = useIntl()
 
@@ -568,40 +569,16 @@ function CategoryIconImage({
 			padding={1}
 		>
 			{category.iconRef?.docId ? (
-				<IconImage
+				<CategoryIconImage
 					projectId={projectId}
 					iconDocumentId={category.iconRef.docId}
-					size={size}
-					alt={t(m.categoryIconAlt, { name: category.name })}
+					imageStyle={{ height: ICON_SIZE, aspectRatio: 1 }}
+					altText={t(m.categoryIconAlt, { name: category.name })}
 				/>
 			) : (
-				<Icon name="material-place" size={size} />
+				<Icon name="material-place" size={20} />
 			)}
 		</CategoryIconContainer>
-	)
-}
-
-function IconImage({
-	alt,
-	iconDocumentId,
-	projectId,
-	size,
-}: {
-	alt: string
-	iconDocumentId: string
-	projectId: string
-	size: number
-}) {
-	const { data: iconUrl } = useIconUrl({
-		projectId,
-		iconId: iconDocumentId,
-		mimeType: 'image/png',
-		size: 'small',
-		pixelDensity: 3,
-	})
-
-	return (
-		<img alt={alt} src={iconUrl} height={size} style={{ aspectRatio: 1 }} />
 	)
 }
 

src/renderer/src/routes/app/projects/$projectId/observations/$observationDocId/index.tsx
@@ -1,12 +1,7 @@
 import { Suspense } from 'react'
-import {
-	useIconUrl,
-	useManyDocs,
-	useSingleDocByDocId,
-} from '@comapeo/core-react'
+import { useManyDocs, useSingleDocByDocId } from '@comapeo/core-react'
 import type { Field, Preset } from '@comapeo/schema'
 import Box from '@mui/material/Box'
-import CircularProgress from '@mui/material/CircularProgress'
 import Divider from '@mui/material/Divider'
 import IconButton from '@mui/material/IconButton'
 import Stack from '@mui/material/Stack'
@@ -16,10 +11,12 @@ import { createFileRoute, notFound, useRouter } from '@tanstack/react-router'
 import { defineMessages, useIntl } from 'react-intl'
 
 import { BLUE_GREY, DARKER_ORANGE } from '../../../../../../colors'
-import { CategoryIconContainer } from '../../../../../../components/category-icon'
+import {
+	CategoryIconContainer,
+	CategoryIconImage,
+} from '../../../../../../components/category-icon'
 import { ErrorBoundary } from '../../../../../../components/error-boundary'
 import { Icon } from '../../../../../../components/icon'
-import { SuspenseImage } from '../../../../../../components/suspense-image'
 import {
 	COMAPEO_CORE_REACT_ROOT_QUERY_KEY,
 	getMatchingCategoryForDocument,
@@ -203,10 +200,11 @@ function RouteComponent() {
 										>
 											{category.iconRef?.docId ? (
 												<CategoryIconImage
-													categoryName={
-														category.name ||
-														t(m.observationCategoryNameFallback)
-													}
+													altText={t(m.categoryIconAlt, {
+														name:
+															category.name ||
+															t(m.observationCategoryNameFallback),
+													})}
 													iconDocumentId={category.iconRef.docId}
 													projectId={projectId}
 												/>
@@ -342,40 +340,6 @@ function RouteComponent() {
 				</Stack>
 			</Box>
 		</Stack>
-	)
-}
-
-function CategoryIconImage({
-	categoryName,
-	iconDocumentId,
-	projectId,
-}: {
-	categoryName: string
-	iconDocumentId: string
-	projectId: string
-}) {
-	const { formatMessage: t } = useIntl()
-
-	const { data: iconUrl } = useIconUrl({
-		projectId,
-		iconId: iconDocumentId,
-		mimeType: 'image/png',
-		size: 'small',
-		pixelDensity: 3,
-	})
-
-	return (
-		<ErrorBoundary
-			getResetKey={() => iconUrl}
-			fallback={() => <Icon name="material-error" color="error" />}
-		>
-			<Suspense fallback={<CircularProgress disableShrink size={30} />}>
-				<SuspenseImage
-					src={iconUrl}
-					alt={t(m.categoryIconAlt, { name: categoryName })}
-				/>
-			</Suspense>
-		</ErrorBoundary>
 	)
 }
 

commit f9f5cc34cb8450923823341c753ec34954ebc80f

fix: fix rendering of category icon image when image fails to load (#231)
Introduced this issue in #230 🤦

src/renderer/src/components/category-icon.tsx
@@ -67,7 +67,17 @@ export function CategoryIconImage({
 	return (
 		<ErrorBoundary
 			getResetKey={() => iconUrl}
-			fallback={() => <Icon name="material-error" color="error" />}
+			fallback={() => (
+				<Box
+					sx={imageStyle}
+					display="flex"
+					justifyContent="center"
+					alignItems="center"
+					flex={1}
+				>
+					<Icon name="material-error" color="error" />
+				</Box>
+			)}
 		>
 			<Suspense fallback={<CircularProgress disableShrink />}>
 				<SuspenseImage src={iconUrl} alt={altText} style={imageStyle} />