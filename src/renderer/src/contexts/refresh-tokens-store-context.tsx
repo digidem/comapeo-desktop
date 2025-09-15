4792e9d9d19bea999011706e71fd1ad6cd8a38d0
Andrew Chou <andrewchou@fastmail.com>
2025-09-15 14:47:33 -0400

    implement context for refresh tokens store

---
+++ src/renderer/src/contexts/refresh-tokens-store-context.tsx
@@ -0,0 +1,55 @@
+import { createContext, use } from 'react'
+import { createStore, useStore } from 'zustand'
+
+export type RefreshTokensStore = ReturnType<typeof createRefreshTokensStore>
+
+type RefreshTokensState = { maps: string }
+
+export function createRefreshTokensStore(opts?: {
+	initialValue: RefreshTokensState
+}) {
+	const store = createStore<RefreshTokensState>(() => {
+		return opts?.initialValue || { maps: Date.now().toString() }
+	})
+
+	const actions = {
+		update: (key: keyof RefreshTokensState) => {
+			store.setState({ [key]: Date.now().toString() })
+		},
+	}
+
+	return { instance: store, actions }
+}
+
+const RefreshTokensStoreContext = createContext<RefreshTokensStore | null>(null)
+export const RefreshTokensStoreProvider = RefreshTokensStoreContext.Provider
+
+function useRefreshTokensStore() {
+	const value = use(RefreshTokensStoreContext)
+
+	if (!value) {
+		throw new Error('Must set up RefreshTokensStoreProvider first')
+	}
+
+	return value
+}
+
+export function useRefreshTokensState(): RefreshTokensState
+export function useRefreshTokensState<T>(
+	selector: (state: RefreshTokensState) => T,
+): T
+export function useRefreshTokensState<T>(
+	selector?: (state: RefreshTokensState) => T,
+) {
+	const store = useRefreshTokensStore()
+	return useStore(store.instance, selector!)
+}
+
+export function useRefreshTokensActions() {
+	const store = useRefreshTokensStore()
+	return store.actions
+}
+
+export function mapsRefreshTokenSelector(state: RefreshTokensState) {
+	return state.maps
+}
