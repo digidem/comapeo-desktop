4b001e3a3323aa46a456d94dbacfccfb873a2f4d
Andrew Chou <andrewchou@fastmail.com>
2025-09-25 15:52:30 -0400

    define accent color (windows only)

--- src/main/app.ts
+++ src/main/app.ts
@@ -281,6 +281,7 @@ function initMainWindow({
 		backgroundColor: '#050F77',
 		titleBarStyle: 'hiddenInset',
 		titleBarOverlay: true,
+		accentColor: '#2348B2',
 		webPreferences: {
 			preload: MAIN_WINDOW_PRELOAD_PATH,
 			additionalArguments: [
