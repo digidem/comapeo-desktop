974c2e99406c15e0303d7ab89fea7c6cbb3d9753
Andrew Chou <andrewchou@fastmail.com>
2025-12-05 12:27:54 -0500

    add eslint rule for relative imports

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,6 +5,7 @@ import { includeIgnoreFile } from '@eslint/compat'
 import js from '@eslint/js'
 import pluginQuery from '@tanstack/eslint-plugin-query'
 import pluginRouter from '@tanstack/eslint-plugin-router'
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths'
 import pluginReactHooks from 'eslint-plugin-react-hooks'
 import pluginReactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig } from 'eslint/config'
@@ -27,9 +28,24 @@ export default defineConfig(
 	includeIgnoreFile(gitignorePath),
 	includeIgnoreFile(gitExcludePath),
 	js.configs.recommended,
+	// Tooling
+	{
+		name: 'Tooling',
+		files: ['**/*.config.{js,ts}', 'scripts/*'],
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.nodeBuiltin,
+				...globals.worker,
+			},
+		},
+	},
 	{
 		name: 'typescript',
 		extends: tseslint.configs.recommended,
+		plugins: {
+			'no-relative-import-paths': noRelativeImportPaths,
+		},
 		rules: {
 			'@typescript-eslint/array-type': ['warn', { default: 'generic' }],
 			// Allow unused vars if prefixed with `_` (https://typescript-eslint.io/rules/no-unused-vars/)
@@ -45,12 +61,74 @@ export default defineConfig(
 					ignoreRestSiblings: true,
 				},
 			],
+			'no-relative-import-paths/no-relative-import-paths': [
+				'error',
+				{
+					allowSameFolder: true,
+					rootDir: 'src/main',
+					prefix: '#main',
+					allowedDepth: 1,
+				},
+			],
+		},
+	},
+	{
+		name: 'electron-main',
+		files: ['src/main/**/*'],
+		plugins: {
+			'no-relative-import-paths': noRelativeImportPaths,
+		},
+		rules: {
+			'no-relative-import-paths/no-relative-import-paths': [
+				'error',
+				{
+					allowSameFolder: true,
+					rootDir: 'src/main',
+					prefix: '#main',
+					allowedDepth: 1,
+				},
+			],
+		},
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.nodeBuiltin,
+				...globals.worker,
+			},
+		},
+	},
+	{
+		name: 'electron-services',
+		files: ['src/services/**/*'],
+		plugins: {
+			'no-relative-import-paths': noRelativeImportPaths,
+		},
+		rules: {
+			'no-relative-import-paths/no-relative-import-paths': [
+				'error',
+				{
+					allowSameFolder: true,
+					rootDir: 'src/services',
+					prefix: '#services',
+					allowedDepth: 1,
+				},
+			],
+		},
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.nodeBuiltin,
+				...globals.worker,
+			},
 		},
 	},
 	// Preload environment
 	{
 		name: 'electron-preload',
 		files: ['src/preload/**/*'],
+		plugins: {
+			'no-relative-import-paths': noRelativeImportPaths,
+		},
 		languageOptions: {
 			globals: {
 				// https://www.electronjs.org/docs/latest/tutorial/tutorial-preload#augmenting-the-renderer-with-a-preload-script
@@ -66,6 +144,15 @@ export default defineConfig(
 			// Preload scripts may use `require()` for accessing Electron APIs
 			// https://www.electronjs.org/docs/latest/tutorial/esm#sandboxed-preload-scripts-cant-use-esm-imports
 			'@typescript-eslint/no-require-imports': 'off',
+			'no-relative-import-paths/no-relative-import-paths': [
+				'error',
+				{
+					allowSameFolder: true,
+					rootDir: 'src/preload',
+					prefix: '#preload',
+					allowedDepth: 1,
+				},
+			],
 		},
 	},
 	// Renderer process
@@ -79,31 +166,26 @@ export default defineConfig(
 			pluginQuery.configs['flat/recommended'],
 			pluginReactRefresh.configs.vite,
 		],
+		plugins: {
+			'no-relative-import-paths': noRelativeImportPaths,
+		},
 		rules: {
 			'react-hooks/exhaustive-deps': 'error',
 			'react-hooks/rules-of-hooks': 'error',
+			'no-relative-import-paths/no-relative-import-paths': [
+				'error',
+				{
+					allowSameFolder: true,
+					rootDir: 'src/renderer',
+					prefix: '#renderer',
+					allowedDepth: 1,
+				},
+			],
 		},
 		languageOptions: {
 			globals: { ...globals.browser },
 			parser: tseslint.parser,
 		},
 		ignores: ['src/renderer/src/generated/routeTree.gen.ts'],
-	},
-	// Node or Node-like processes
-	{
-		name: 'node',
-		files: [
-			'**/*.config.{js,ts}',
-			'src/main/**/*',
-			'src/services/**/*',
-			'scripts/*',
-		],
-		languageOptions: {
-			globals: {
-				...globals.node,
-				...globals.nodeBuiltin,
-				...globals.worker,
-			},
-		},
 	},
 )
