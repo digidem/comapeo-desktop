eed128f0cf638a3758ecf2816560e63a4c58d3c2
Andrew Chou <andrewchou@fastmail.com>
2026-01-09 22:35:47 -0500

    update beforeLoad for device invite pages

--- src/renderer/src/routes/app/projects/$projectId_/invite/devices/$deviceId/route.tsx
+++ src/renderer/src/routes/app/projects/$projectId_/invite/devices/$deviceId/route.tsx
@@ -1,6 +1,7 @@
 import { Outlet, createFileRoute, redirect } from '@tanstack/react-router'
 
 import { GenericRoutePendingComponent } from '../../../../../../../components/generic-route-pending-component'
+import { COMAPEO_CORE_REACT_ROOT_QUERY_KEY } from '../../../../../../../lib/comapeo.ts'
 
 export const Route = createFileRoute(
 	'/app/projects/$projectId_/invite/devices/$deviceId',
@@ -8,7 +9,7 @@ export const Route = createFileRoute(
 	// NOTE: We want to make sure that the device of interest is actually still detected by us and connected.
 	// If it's not, then we redirect to the page that lists the discovered devices.
 	beforeLoad: async ({ context, params }) => {
-		const { clientApi, projectApi } = context
+		const { clientApi, projectApi, queryClient } = context
 		const { deviceId, projectId } = params
 		const peers = await clientApi.listLocalPeers()
 
@@ -22,7 +23,22 @@ export const Route = createFileRoute(
 			})
 		}
 
-		const member = await projectApi.$member.getById(deviceId).catch(() => null)
+		const member = await queryClient
+			.fetchQuery({
+				queryKey: [
+					COMAPEO_CORE_REACT_ROOT_QUERY_KEY,
+					'projects',
+					projectId,
+					'members',
+					deviceId,
+				],
+				queryFn: async () => {
+					return projectApi.$member.getById(deviceId)
+				},
+			})
+			.catch(() => {
+				return null
+			})
 
 		// TODO: Do not redirect if member left project?
 		if (member) {
