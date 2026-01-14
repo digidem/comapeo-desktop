74820caa3a7fa23d541af086996f265dbb985824
Andrew Chou <andrewchou@fastmail.com>
2026-01-14 13:26:27 -0500

    update translated languages script

--- scripts/generate-translated-languages-list.js
+++ scripts/generate-translated-languages-list.js
@@ -1,7 +1,6 @@
 // Creates a JSON file that lists the language tags for which we have renderer translations.
 // This is done so that this info is known at compile time instead of being calculated during run time.
-import fs, { readFileSync } from 'node:fs'
-import { glob } from 'node:fs/promises'
+import { readFile, readdir, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import * as v from 'valibot'
@@ -18,14 +17,6 @@ const SUPPORTED_LANGUAGE_TAGS = /** @type { SupportedLanguageTag[] } */ (
 	Object.keys(SUPPORTED_LANGUAGES)
 )
 
-/**
- * @typedef { {
- * 	languageTag: SupportedLanguageTag
- * 	nativeName: string
- * 	englishName: string
- * } } UsableLanguage
- */
-
 const SupportedLanguageTagSchema = v.union(
 	SUPPORTED_LANGUAGE_TAGS.map((t) => v.literal(t)),
 )
@@ -39,39 +30,67 @@ const RENDERER_MESSAGES_DIR = fileURLToPath(
  */
 const translatedLanguages = []
 
-for await (const entry of glob('*.json', { cwd: RENDERER_MESSAGES_DIR })) {
-	const match = entry.match(/(?<language>.+)\.json/)
+const localeFolders = await readdir(RENDERER_MESSAGES_DIR, {
+	withFileTypes: true,
+})
 
-	const matchedLanguage = match?.groups?.language
-
-	if (!matchedLanguage) {
-		throw new Error(`Could not extract language tag from: ${entry}`)
+for (const folder of localeFolders) {
+	if (!folder.isDirectory()) {
+		continue
 	}
 
-	if (!v.is(SupportedLanguageTagSchema, matchedLanguage)) {
+	const locale = folder.name
+
+	if (!v.is(SupportedLanguageTagSchema, locale)) {
 		console.warn(
-			`Translated language tag "${matchedLanguage}" is not listed as a supported language.\n\n
+			`Translated language tag "${locale}" is not listed as a supported language.\n\n
             Update languages.json with the relevant information in order for it to be displayed as an option in the application.`,
 		)
 		continue
 	}
 
-	const messages = JSON.parse(
-		readFileSync(join(RENDERER_MESSAGES_DIR, entry), {
+	const [coreMessages, primaryMessages, secondaryMessages] = await Promise.all([
+		readFile(join(RENDERER_MESSAGES_DIR, locale, 'core.json'), {
 			encoding: 'utf-8',
-		}),
-	)
+		}).then(JSON.parse),
+
+		readFile(join(RENDERER_MESSAGES_DIR, locale, 'primary.json'), {
+			encoding: 'utf-8',
+		})
+			.then(JSON.parse)
+			.catch(() => {
+				console.warn(`${locale}: no primary messages found`)
+				return undefined
+			}),
+
+		readFile(join(RENDERER_MESSAGES_DIR, locale, 'primary.json'), {
+			encoding: 'utf-8',
+		})
+			.then(JSON.parse)
+			.catch(() => {
+				console.warn(`${locale}: no secondary messages found`)
+				return undefined
+			}),
+	])
+
+	const messages = {
+		...coreMessages,
+		...primaryMessages,
+		...secondaryMessages,
+	}
+
+	// TODO: This will need to be adjusted based on some defined criteria
 
 	// If a language is added to Crowdin but has no translated messages,
 	// Crowdin still creates an empty file. We do not include as a selectable language.
 	if (Object.keys(messages).length === 0) {
 		console.warn(
-			`No translated messages for "${matchedLanguage}". Not including as a translated language for the app to use.`,
+			`No translated messages for "${locale}". Not including as a translated language for the app to use.`,
 		)
 		continue
 	}
 
-	translatedLanguages.push(matchedLanguage)
+	translatedLanguages.push(locale)
 }
 
 const OUTPUT_FILE = fileURLToPath(
@@ -81,6 +100,6 @@ const OUTPUT_FILE = fileURLToPath(
 	),
 )
 
-fs.writeFileSync(OUTPUT_FILE, JSON.stringify(translatedLanguages))
+await writeFile(OUTPUT_FILE, JSON.stringify(translatedLanguages))
 
 console.log(`✅ Generated file at ${OUTPUT_FILE}`)
