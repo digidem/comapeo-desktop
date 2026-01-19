digidem/comapeo-desktop

commit c24a6a2655b3508f68c8e1d8b043a89dbe00e094

fix: fix redirect issue when leaving a project (#485)
Closes #484 .

This was a gnarly one. This is what was happening during the leave
project flow from a tanstack query perspective:

1. Click confirm button to leave project, which calls the leave project
mutation (part of core-react).
2. In core-react when the mutation succeeds, it [invalidates all queries
related to
projects](https://github.com/digidem/comapeo-core-react/blob/7d148b47230ff31cfb3378d881a111891db7ae58/src/lib/react-query/projects.ts#L337-L339).
3. We then attempt to navigate to the onboarding.
4. In the `beforeLoad` for the onboarding route, we [check to
see](https://github.com/digidem/comapeo-desktop/blob/4a6bc583c09e7a0f8ac03a6bc34968cd0f96644f/src/renderer/src/routes/onboarding/route.tsx#L24-L39)
if we're already on a project, as we'd rather redirect them to a valid
project than to let the user access the onboarding.
- We ideally wouldn't need to do this and instead would redirect to some
project selection page, but that's a separate concern.

The problem was that in (4), we use
[queryClient.ensureQueryData](https://github.com/digidem/comapeo-desktop/blob/4a6bc583c09e7a0f8ac03a6bc34968cd0f96644f/src/renderer/src/routes/onboarding/route.tsx#L24)
to get the list of projects the user is still a part of. However, this
[method](https://tanstack.com/query/latest/docs/reference/QueryClient#queryclientensurequerydata)
looks in the query cache for an existing value and only resorts to
refetching if it doesn't already exist. In our case a cached value
already exists but it's the value prior to leaving successfully, meaning
that we were redirecting back to the project that we just left, hence
why nothing seems to happen and things are broken on subsequent attempts
to leave. The [`revalidateIfStale`
option](https://tanstack.com/query/latest/docs/reference/QueryClient#queryclientensurequerydata)
is almost exactly what we want in this situation, but it will still

src/renderer/src/routes/app/projects/$projectId_/invite/devices/$deviceId/route.tsx
@@ -1,6 +1,7 @@
 import { Outlet, createFileRoute, redirect } from '@tanstack/react-router'
 
 import { GenericRoutePendingComponent } from '../../../../../../../components/generic-route-pending-component'
+import { COMAPEO_CORE_REACT_ROOT_QUERY_KEY } from '../../../../../../../lib/comapeo.ts'
 
 export const Route = createFileRoute(
 	'/app/projects/$projectId_/invite/devices/$deviceId',
@@ -8,7 +9,7 @@ export const Route = createFileRoute(
 	// NOTE: We want to make sure that the device of interest is actually still detected by us and connected.
 	// If it's not, then we redirect to the page that lists the discovered devices.
 	beforeLoad: async ({ context, params }) => {
-		const { clientApi, projectApi } = context
+		const { clientApi, projectApi, queryClient } = context
 		const { deviceId, projectId } = params
 		const peers = await clientApi.listLocalPeers()
 
@@ -22,7 +23,22 @@ export const Route = createFileRoute(
 			})
 		}
 
-		const member = await projectApi.$member.getById(deviceId).catch(() => null)
+		const member = await queryClient
+			.fetchQuery({
+				queryKey: [
+					COMAPEO_CORE_REACT_ROOT_QUERY_KEY,
+					'projects',
+					projectId,
+					'members',
+					deviceId,
+				],
+				queryFn: async () => {
+					return projectApi.$member.getById(deviceId)
+				},
+			})
+			.catch(() => {
+				return null
+			})
 
 		// TODO: Do not redirect if member left project?
 		if (member) {

src/renderer/src/routes/app/projects/$projectId_/team/$deviceId.tsx
@@ -14,7 +14,7 @@ import CircularProgress from '@mui/material/CircularProgress'
 import IconButton from '@mui/material/IconButton'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createFileRoute, useRouter } from '@tanstack/react-router'
 import { defineMessages, useIntl } from 'react-intl'
 
@@ -83,6 +83,8 @@ function RouteComponent() {
 
 	const activeProjectIdActions = useActiveProjectIdActions()
 
+	const queryClient = useQueryClient()
+
 	const leaveProjectAndNavigate = useMutation({
 		mutationKey: LEAVE_PROJECT_AND_NAVIGATE_MUTATION_KEY,
 		mutationFn: async ({ projectId }: { projectId: string }) => {
@@ -92,14 +94,23 @@ function RouteComponent() {
 			// TODO: Ideally we don't autonavigate to some arbitrary project.
 			// Instead we should allow the user to choose which project to enter.
 			// Okay to do for now because we don't allow joining another project after the onboarding right now.
-			const projects = await clientApi.listProjects()
+
+			// NOTE: We use fetchQuery here in order to update the query cache for the relevant query before
+			// attempting to do a navigation, which prevents an issue with an incorrect redirect based on stale cache data
+			// in the relevant route's beforeLoad callback.
+			const projects = await queryClient.fetchQuery({
+				queryKey: [COMAPEO_CORE_REACT_ROOT_QUERY_KEY, 'projects'],
+				queryFn: async () => {
+					return clientApi.listProjects()
+				},
+			})
 
 			const projectToNavigateTo = projects.find(
 				(p) => p.projectId !== variables.projectId,
 			)
 
 			if (projectToNavigateTo) {
-				router.navigate({
+				await router.navigate({
 					to: '/app/projects/$projectId',
 					params: { projectId: projectToNavigateTo.projectId },
 				})

commit 1e42b78e0553181c0278c409a176ade45cad16c7

fix: fix error screen after device accepts invite sent by us (#488)
Fixes a regression introduced by #485 . Repro steps:

1. Invite mobile device from desktop
2. On mobile device, accept invite.
3. On desktop, observe the following:

<img width="600" alt="image"
src="https://github.com/user-attachments/assets/df79646c-f8ff-4992-b092-a33ee8b2bbbc"
/>
    
Still a little miffed at the behavior, as it seems to be some sort of
race condition or a misuse of router/query. Haven't really wrapped my
head around where the actual issue lies, but guessing it's on our end.

src/renderer/src/routes/app/projects/$projectId_/invite/devices/$deviceId/route.tsx
@@ -1,7 +1,6 @@
 import { Outlet, createFileRoute, redirect } from '@tanstack/react-router'
 
 import { GenericRoutePendingComponent } from '../../../../../../../components/generic-route-pending-component'
-import { COMAPEO_CORE_REACT_ROOT_QUERY_KEY } from '../../../../../../../lib/comapeo.ts'
 
 export const Route = createFileRoute(
 	'/app/projects/$projectId_/invite/devices/$deviceId',
@@ -9,7 +8,7 @@ export const Route = createFileRoute(
 	// NOTE: We want to make sure that the device of interest is actually still detected by us and connected.
 	// If it's not, then we redirect to the page that lists the discovered devices.
 	beforeLoad: async ({ context, params }) => {
-		const { clientApi, projectApi, queryClient } = context
+		const { clientApi, projectApi } = context
 		const { deviceId, projectId } = params
 		const peers = await clientApi.listLocalPeers()
 
@@ -23,22 +22,12 @@ export const Route = createFileRoute(
 			})
 		}
 
-		const member = await queryClient
-			.fetchQuery({
-				queryKey: [
-					COMAPEO_CORE_REACT_ROOT_QUERY_KEY,
-					'projects',
-					projectId,
-					'members',
-					deviceId,
-				],
-				queryFn: async () => {
-					return projectApi.$member.getById(deviceId)
-				},
-			})
-			.catch(() => {
-				return null
-			})
+		// NOTE: This is intentionally a plain call instead of something that's wrapped in
+		// `queryClient.ensureQueryData()`, `queryClient.fetchQuery()`, etc.
+		// This prevents an issue with showing the success state when another device accepts an invite sent by us.
+		const member = await projectApi.$member.getById(deviceId).catch(() => {
+			return null
+		})
 
 		// TODO: Do not redirect if member left project?
 		if (member) {