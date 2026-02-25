3d3e3730236fe3ee810a74488546dbc4c5464eec
Andrew Chou <andrewchou@fastmail.com>
2026-02-25 14:00:48 -0500

    update index page redirect behavior

--- src/renderer/src/routes/index.tsx
+++ src/renderer/src/routes/index.tsx
@@ -1,23 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-	beforeLoad: async ({ context }) => {
-		const { activeProjectIdStore } = context
-
-		const activeProjectId = activeProjectIdStore.instance.getState()
-
-		// TODO: Ideally only do this if last visited page was a project-related page
-		if (activeProjectId) {
-			throw Route.redirect({
-				to: '/app/projects/$projectId',
-				params: { projectId: activeProjectId },
-				replace: true,
-			})
-		}
-
-		throw Route.redirect({
-			to: '/app',
-			replace: true,
-		})
+	beforeLoad: () => {
+		// TODO: Ideally navigate to the specific project page if last viewed page before app close
+		// is a project-specific page
+		throw Route.redirect({ to: '/app', replace: true })
 	},
 })
