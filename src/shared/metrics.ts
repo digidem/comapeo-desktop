608f4aaa45fdc55b2cecb61528c3a1ec4ae0260f
Andrew Chou <andrewchou@fastmail.com>
2026-04-06 10:52:09 -0400

    remove minimum value requirement for updatedAt

--- src/shared/metrics.ts
+++ src/shared/metrics.ts
@@ -3,7 +3,7 @@ import * as v from 'valibot'
 export const AppUsageMetricsSchema = v.object({
 	status: v.union([v.literal('disabled'), v.literal('enabled')]),
 	askCount: v.pipe(v.number(), v.integer(), v.minValue(0)),
-	updatedAt: v.pipe(v.number(), v.integer(), v.minValue(0)),
+	updatedAt: v.pipe(v.number(), v.integer()),
 })
 
 export type AppUsageMetrics = v.InferOutput<typeof AppUsageMetricsSchema>
