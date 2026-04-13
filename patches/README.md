ff9c17713613a9d14bfbaed69f40fbcb764828bb
Andrew Chou <andrewchou@fastmail.com>
2026-04-13 11:52:32 -0400

    update @comapeo/map-server

--- patches/README.md
+++ patches/README.md
@@ -8,9 +8,3 @@ fixes.
 ### [Do not watch fallback map patch when setting up SMP server plugin](./@comapeo+core+7.0.1+001+fix-smp-fallback-map-setup.patch)
 
 By default, core sets up a file watcher for the `fallbackMapPath` option that's provided when instantiating `MapeoManager`. This does not work when packaging the app as an ASAR file (via Electron Forge) because watching a file within the ASAR directory is not possible. Instead, we change the setup so that it does not try to watch the file and instead make the assumption that the file always exists on instantiation, which is generally the case in CoMapeo Desktop (for now).
-
-## @comapeo/map-server
-
-### [Fix CORS headers for PUT methods](./@comapeo+map-server+1.1.0+001+fix-put-cors-error.patch)
-
-Most likely only an issue in development, but when performing any PUT operations (e.g. uploading a custom map), it fails due to a CORS error.
--- src/renderer/src/components/map.tsx
+++ src/renderer/src/components/map.tsx
@@ -1,40 +1,7 @@
-import { type Ref } from 'react'
-import type { ResourceType } from 'maplibre-gl'
-import {
-	isMapboxURL,
-	transformMapboxUrl,
-} from 'maplibregl-mapbox-request-transformer'
-import {
-	Map as ReactMapLibre,
-	type MapProps,
-	type MapRef,
-} from 'react-map-gl/maplibre'
+import { Map as ReactMapLibre, type MapProps } from 'react-map-gl/maplibre'
 
 import 'maplibre-gl/dist/maplibre-gl.css'
 
-export function Map({
-	ref,
-	reuseMaps = true,
-	...rest
-}: Omit<MapProps, 'transformRequest'> & { ref?: Ref<MapRef> }) {
-	return (
-		<ReactMapLibre
-			{...rest}
-			transformRequest={transformRequest}
-			ref={ref}
-			reuseMaps={reuseMaps}
-		/>
-	)
-}
-
-function transformRequest(url: string, resourceType?: ResourceType) {
-	if (isMapboxURL(url)) {
-		return transformMapboxUrl(
-			url,
-			resourceType,
-			import.meta.env.VITE_MAPBOX_ACCESS_TOKEN,
-		)
-	}
-
-	return { url }
+export function Map({ reuseMaps = true, ...rest }: MapProps) {
+	return <ReactMapLibre {...rest} reuseMaps={reuseMaps} />
 }
