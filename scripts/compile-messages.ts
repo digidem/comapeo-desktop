233d3a3a3d06502b2deb36e5442bbcc896b3af38
Andrew Chou <andrewchou@fastmail.com>
2026-04-14 15:18:01 -0400

    update compile and extract scripts and use in package.json scripts

--- scripts/compile-messages.ts
+++ scripts/compile-messages.ts
@@ -1,45 +1,50 @@
 import { mkdirSync, readdirSync, writeFileSync } from 'node:fs'
 import { join, relative } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { styleText } from 'node:util'
+import { parseArgs, styleText } from 'node:util'
 import { compile } from '@formatjs/cli-lib'
+
+const { values } = parseArgs({
+	strict: true,
+	options: { type: { type: 'string' } },
+})
+
+if (values.type !== 'main' && values.type !== 'renderer') {
+	throw new Error('type must be either main or renderer')
+}
 
 const PROJECT_ROOT = fileURLToPath(new URL('..', import.meta.url))
 const MESSAGES_DIR = join(PROJECT_ROOT, 'messages')
 const TRANSLATIONS_DIR = join(PROJECT_ROOT, 'translations')
 
-await Promise.all([compileMessages('main'), compileMessages('renderer')])
+const sourceDir = join(MESSAGES_DIR, values.type)
+const outputDir = join(TRANSLATIONS_DIR, values.type)
 
-async function compileMessages(type: 'main' | 'renderer') {
-	const sourceDir = join(MESSAGES_DIR, type)
-	const outputDir = join(TRANSLATIONS_DIR, type)
+mkdirSync(outputDir, { recursive: true })
 
-	mkdirSync(outputDir, { recursive: true })
+const directories = readdirSync(sourceDir, {
+	withFileTypes: true,
+}).filter((d) => d.isDirectory())
 
-	const directories = readdirSync(sourceDir, {
-		withFileTypes: true,
-	}).filter((d) => d.isDirectory())
+for (const d of directories) {
+	const languageCode = d.name
 
-	for (const d of directories) {
-		const languageCode = d.name
-
-		const compiled = await compile(
-			[
-				join(d.parentPath, d.name, 'primary.json'),
-				join(d.parentPath, d.name, 'secondary.json'),
-			],
-			{ ast: true, format: 'crowdin' },
-		)
-
-		const outputFile = join(outputDir, `${languageCode}.json`)
-
-		writeFileSync(outputFile, compiled, 'utf-8')
-	}
-
-	console.log(
-		styleText(
-			['green'],
-			`Compiled messages from ${relative(PROJECT_ROOT, sourceDir)} to ${relative(PROJECT_ROOT, outputDir)}`,
-		),
+	const compiled = await compile(
+		[
+			join(d.parentPath, d.name, 'primary.json'),
+			join(d.parentPath, d.name, 'secondary.json'),
+		],
+		{ ast: true, format: 'crowdin' },
 	)
+
+	const outputFile = join(outputDir, `${languageCode}.json`)
+
+	writeFileSync(outputFile, compiled, 'utf-8')
 }
+
+console.log(
+	styleText(
+		['green'],
+		`Compiled messages from ${relative(PROJECT_ROOT, sourceDir)} to ${relative(PROJECT_ROOT, outputDir)}`,
+	),
+)
--- scripts/extract-messages.ts
+++ scripts/extract-messages.ts
@@ -1,82 +1,72 @@
 import { mkdirSync } from 'node:fs'
 import { glob, writeFile } from 'node:fs/promises'
-import { join } from 'node:path'
+import { join, relative } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import { parseArgs, styleText } from 'node:util'
 import { extract } from '@formatjs/cli-lib'
+
+const { values } = parseArgs({
+	strict: true,
+	options: { type: { type: 'string' } },
+})
+
+if (values.type !== 'main' && values.type !== 'renderer') {
+	throw new Error('type must be either main or renderer')
+}
 
 const PROJECT_ROOT_DIR = fileURLToPath(new URL('..', import.meta.url))
 
-const MESSAGES_DIR_MAIN = fileURLToPath(
-	new URL('../messages/main', import.meta.url),
-)
-const MESSAGES_DIR_RENDERER = fileURLToPath(
-	new URL('../messages/renderer', import.meta.url),
+const MESSAGES_DIR = fileURLToPath(
+	new URL(`../messages/${values.type}`, import.meta.url),
 )
 
 const DEFAULT_LANGUAGE = 'en-US' as const
 
-const OUTPUT_DIR_MAIN = join(MESSAGES_DIR_MAIN, DEFAULT_LANGUAGE)
-const OUTPUT_DIR_RENDERER = join(MESSAGES_DIR_RENDERER, DEFAULT_LANGUAGE)
+const OUTPUT_DIR = join(MESSAGES_DIR, DEFAULT_LANGUAGE)
 
-mkdirSync(OUTPUT_DIR_MAIN, { recursive: true })
-mkdirSync(OUTPUT_DIR_RENDERER, { recursive: true })
+mkdirSync(OUTPUT_DIR, { recursive: true })
 
-const [main, renderer] = await Promise.all([extractMain(), extractRenderer()])
+const sourceFiles = (
+	await Array.fromAsync(
+		glob(`src/${values.type}/**/*.{js,jsx,ts,tsx}`, {
+			cwd: PROJECT_ROOT_DIR,
+			exclude: [`src/${values.type}/**/*.d.ts`],
+			withFileTypes: true,
+		}),
+	)
+)
+	.filter((d) => d.isFile())
+	.map((d) => join(d.parentPath, d.name))
+
+const extracted = await extract(sourceFiles, {
+	ast: true,
+	format: 'crowdin',
+	throws: true,
+})
+
+const { primary, secondary } = getCategorizedMessages(JSON.parse(extracted))
 
 await Promise.all([
 	writeFile(
-		join(OUTPUT_DIR_MAIN, 'primary.json'),
-		JSON.stringify(main.primary, undefined, 2),
+		join(OUTPUT_DIR, 'primary.json'),
+		JSON.stringify(primary, undefined, 2),
 		'utf-8',
 	),
 	writeFile(
-		join(OUTPUT_DIR_MAIN, 'secondary.json'),
-		JSON.stringify(main.secondary, undefined, 2),
-		'utf-8',
-	),
-	writeFile(
-		join(OUTPUT_DIR_RENDERER, 'primary.json'),
-		JSON.stringify(renderer.primary, undefined, 2),
-		'utf-8',
-	),
-	writeFile(
-		join(OUTPUT_DIR_RENDERER, 'secondary.json'),
-		JSON.stringify(renderer.secondary, undefined, 2),
+		join(OUTPUT_DIR, 'secondary.json'),
+		JSON.stringify(secondary, undefined, 2),
 		'utf-8',
 	),
 ])
 
-async function extractMain() {
-	const files = await Array.fromAsync(
-		glob('src/main/**/*.{js,ts}', {
-			cwd: PROJECT_ROOT_DIR,
-			exclude: ['src/main/**/*.d.ts'],
-		}),
-	)
+console.log(
+	styleText(
+		['green'],
+		`Extracted messages from ${join('src', values.type)} to ${relative(PROJECT_ROOT_DIR, OUTPUT_DIR)}`,
+	),
+)
 
-	const extracted = JSON.parse(
-		await extract(files, { ast: true, format: 'crowdin', throws: true }),
-	)
-
-	return getCategorizedMessages(extracted)
-}
-
-async function extractRenderer() {
-	const files = await Array.fromAsync(
-		glob('src/renderer/**/*.{js,jsx,ts,tsx}', {
-			cwd: PROJECT_ROOT_DIR,
-			exclude: ['src/renderer/**/*.d.ts'],
-		}),
-	)
-
-	const extracted = JSON.parse(
-		await extract(files, { ast: true, format: 'crowdin' }),
-	)
-
-	return getCategorizedMessages(extracted)
-}
-
-async function getCategorizedMessages(messages: Record<string, unknown>) {
+function getCategorizedMessages(messages: Record<string, unknown>) {
 	const primary = {} as Record<string, unknown>
 	const secondary = {} as Record<string, unknown>
 
