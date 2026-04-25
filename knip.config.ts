7c3bf2c1546810f064acf435a8da66c0afc2ba79
Andrew Chou <andrewchou@fastmail.com>
2026-04-25 13:46:44 +0100

    update knip

--- knip.config.ts
+++ knip.config.ts
@@ -16,7 +16,6 @@ export default {
 		'tests-e2e/**/*.{js,ts}',
 		'*.{js,ts}',
 	],
-	ignore: ['src/renderer/src/generated/*'],
 	ignoreDependencies: ['@fontsource-variable/rubik'],
 	ignoreExportsUsedInFile: {
 		interface: true,
