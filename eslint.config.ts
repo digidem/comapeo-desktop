b0bd73825d50d8a880899ff87243c1190d31d9dc
Andrew Chou <andrewchou@fastmail.com>
2026-05-27 16:11:10 -0400

    chore: set up React Compiler (#593)
    Integrates [React
    Compiler](https://react.dev/reference/react-compiler/configuration).
    Resulted in one notable regression related to `@tanstack/react-virtual`
    being an incompatible library, but otherwise have not encountered any
    other issues.

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,6 +4,7 @@ import js from '@eslint/js'
 import pluginQuery from '@tanstack/eslint-plugin-query'
 import pluginRouter from '@tanstack/eslint-plugin-router'
 import formatjs from 'eslint-plugin-formatjs'
+import reactHooks from 'eslint-plugin-react-hooks'
 import { reactRefresh } from 'eslint-plugin-react-refresh'
 import { defineConfig, includeIgnoreFile } from 'eslint/config'
 import globals from 'globals'
@@ -70,6 +71,9 @@ export default defineConfig(
 				// https://github.com/ArnaudBarre/eslint-plugin-react-refresh/releases/tag/v0.5.0
 				extraHOCs: ['createFileRoute', 'createRootRouteWithContext'],
 			}),
+			reactHooks.configs.flat.recommended,
+			// NOTE: Must come after eslint-plugin-react-hooks config
+			react.configs['disable-conflict-eslint-plugin-react-hooks'],
 		],
 		rules: {
 			'@eslint-react/exhaustive-deps': 'error',
