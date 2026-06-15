a5b90addc375ad62c2a25cc8fb9ce632fe21bab0
Andrew Chou <andrewchou@fastmail.com>
2026-06-15 15:44:22 -0500

    wip: advanced filter ui

--- src/renderer/src/routes/app/projects/$projectId/_main-tabs/-shared.ts
+++ src/renderer/src/routes/app/projects/$projectId/_main-tabs/-shared.ts
@@ -1,3 +1,12 @@
+import type { Observation, Preset, Track } from '@comapeo/core/schema.js'
+import {
+	isAfter,
+	isBefore,
+	isEqual,
+	startOfDay,
+	startOfMonth,
+	subDays,
+} from 'date-fns'
 import * as v from 'valibot'
 
 export const HighlightedDocumentSchema = v.object({
@@ -7,3 +16,71 @@ export const HighlightedDocumentSchema = v.object({
 })
 
 export type HighlightedDocument = v.InferInput<typeof HighlightedDocumentSchema>
+
+export type DateFilter =
+	| { type: 'range'; start: Date; end: Date }
+	| { type: 'same'; unit: 'month' | 'year' }
+	| { type: 'relative'; unit: 'days'; value: number }
+
+type DateRange = { start: Date | null; end: Date }
+
+export function dateFilterToDateRange(
+	dateFilter: DateFilter,
+	now: Date,
+): DateRange {
+	switch (dateFilter.type) {
+		case 'range': {
+			return { start: dateFilter.start, end: dateFilter.end }
+		}
+		case 'relative': {
+			return {
+				start: subDays(startOfDay(now), dateFilter.value),
+				end: now,
+			}
+		}
+		case 'same': {
+			return { start: startOfMonth(now), end: now }
+		}
+	}
+}
+
+export function isDocumentIncludedByFilters(
+	item: { document: Observation | Track; category?: Preset },
+	filters: { categories: Array<Preset>; date?: DateRange },
+) {
+	const categoryIdsToInclude = new Set(filters.categories.map((c) => c.docId))
+
+	if (filters.date) {
+		const { createdAt } = item.document
+
+		const isBeforeEndDateInclusive =
+			isEqual(createdAt, filters.date.end) ||
+			isBefore(createdAt, filters.date.end)
+
+		if (!isBeforeEndDateInclusive) {
+			return false
+		}
+
+		if (filters.date.start) {
+			const isAfterStartDateInclusive =
+				isEqual(createdAt, filters.date.start) ||
+				isAfter(createdAt, filters.date.start)
+
+			if (!isAfterStartDateInclusive) {
+				return false
+			}
+		}
+	}
+
+	if (categoryIdsToInclude.size > 0) {
+		if (!item.category?.docId) {
+			return false
+		}
+
+		if (!categoryIdsToInclude.has(item.category.docId)) {
+			return false
+		}
+	}
+
+	return true
+}
