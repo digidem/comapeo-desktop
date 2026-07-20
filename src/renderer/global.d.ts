7ac2b259707a62c3b3f96ee690d24a09caa3809f
Andrew Chou <andrewchou@fastmail.com>
2026-07-20 15:45:24 -0400

    chore: address install issues in release workflows (#630)
    Think the TypeScript introduction in #629 causes the failure seen in
    https://github.com/digidem/comapeo-desktop/actions/runs/29764026090/job/88425560479.
    I was running into this locally but was able to bypass it by running
    `--force` (yeah I know, not safe...). This PR updates a couple of things
    to potentially mitigate this:
    
    1. Vendor the `network-information-types` module. It's no longer being
    maintained anyways and there isn't much to vendor. Somehow the
    TypeScript peer dep of `>= 3.0.0` is contributing to the issue.
    
    2. Make sure that the affected workflows are using an aligned npm
    version. The `check-pr-to-release` and `release` workflows do an npm
    install of `semver` using a misaligned npm version to what the project
    generally uses. Ideally this shouldn't cause issues but just to be safe,
    I updated the workflows to make sure to install the relevant npm version
    before doing the steps that make use of the `semver` module.

--- src/renderer/global.d.ts
+++ src/renderer/global.d.ts
@@ -1,13 +1,64 @@
-/// <reference types="network-information-types/dist-types/" />
-
 import type { MessageDescriptor } from 'react-intl'
 
-import { type RuntimeApi } from '../preload/runtime'
+import { type RuntimeApi } from '../preload/runtime.js'
 
 declare global {
 	// Make changes here whenever you expose new things in the preload/ using exposeInMainWorld
 	interface Window {
 		runtime: RuntimeApi
+	}
+
+	// ---
+	// Adjusted vendored version of https://github.com/lacolaco/network-information-types/blob/17d082376d27ee834dc277dfa8e42c9de20440e7/index.d.ts
+	// ---
+
+	// W3C Spec Draft http://wicg.github.io/netinfo/
+	// Edition: Draft Community Group Report 20 February 2019
+
+	// http://wicg.github.io/netinfo/#navigatornetworkinformation-interface
+	interface Navigator {
+		readonly connection?: NetworkInformation
+	}
+	interface WorkerNavigator {
+		readonly connection?: NetworkInformation
+	}
+
+	// http://wicg.github.io/netinfo/#connection-types
+	type ConnectionType =
+		| 'bluetooth'
+		| 'cellular'
+		| 'ethernet'
+		| 'mixed'
+		| 'none'
+		| 'other'
+		| 'unknown'
+		| 'wifi'
+		| 'wimax'
+
+	// http://wicg.github.io/netinfo/#effectiveconnectiontype-enum
+	type EffectiveConnectionType = '2g' | '3g' | '4g' | 'slow-2g'
+
+	// http://wicg.github.io/netinfo/#dom-megabit
+	type Megabit = number
+	// http://wicg.github.io/netinfo/#dom-millisecond
+	type Millisecond = number
+
+	// http://wicg.github.io/netinfo/#networkinformation-interface
+	interface NetworkInformation extends EventTarget {
+		// http://wicg.github.io/netinfo/#type-attribute
+		readonly type?: ConnectionType
+		// http://wicg.github.io/netinfo/#effectivetype-attribute
+		readonly effectiveType?: EffectiveConnectionType
+		// http://wicg.github.io/netinfo/#downlinkmax-attribute
+		readonly downlinkMax?: Megabit
+		// http://wicg.github.io/netinfo/#downlink-attribute
+		readonly downlink?: Megabit
+		// http://wicg.github.io/netinfo/#rtt-attribute
+		readonly rtt?: Millisecond
+		// http://wicg.github.io/netinfo/#savedata-attribute
+		readonly saveData?: boolean
+		// http://wicg.github.io/netinfo/#handling-changes-to-the-underlying-connection
+		onchange?: EventListener
 	}
 }
 
