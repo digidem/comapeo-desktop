c5281b60b5972940d46dedd5b4c64eb7e2f881b3
Andrew Chou <andrewchou@fastmail.com>
2026-09-21 15:43:07 -0400

    update list spec

--- tests-e2e/specs/app/project/list.spec.ts
+++ tests-e2e/specs/app/project/list.spec.ts
@@ -1,7 +1,5 @@
-import { hexToRgb } from '@mui/material/styles'
 import { expect } from 'playwright/test'
 
-import { COMAPEO_BLUE } from '../../../../src/renderer/src/colors.ts'
 import {
 	setup,
 	simulateCreateProject,
@@ -31,8 +29,7 @@ test.describe('Main panel (coordinator)', () => {
 			})
 
 			await page
-				.getByRole('navigation', { name: 'App navigation', exact: true })
-				.getByRole('button', {
+				.getByRole('link', {
 					name: `Go to project ${projectParams.projectName}.`,
 					exact: true,
 				})
@@ -47,10 +44,7 @@ test.describe('Main panel (coordinator)', () => {
 					.getByRole('navigation', { name: 'Project navigation', exact: true })
 					.getByRole('link', { name: 'List', exact: true })
 
-				await expect(projectListNavLink).toHaveCSS(
-					'color',
-					hexToRgb(COMAPEO_BLUE),
-				)
+				await expect(projectListNavLink).toHaveAttribute('aria-current', 'page')
 			}
 
 			const main = page.getByRole('main')
@@ -71,86 +65,6 @@ test.describe('Main panel (coordinator)', () => {
 			await expect(
 				main.getByRole('link', { name: 'Invite Device', exact: true }),
 			).toBeVisible()
-		} finally {
-			// 3. Cleanup
-			await electronApp.close()
-			cleanup()
-		}
-	})
-
-	test('Project info popup', async ({ appInfo, projectParams, userParams }) => {
-		const { cleanup, launchApp } = await setup()
-		const electronApp = await launchApp({ appInfo })
-
-		try {
-			const page = await electronApp.firstWindow()
-
-			// 1. Simulate onboarding
-			await simulateOnboarding({
-				deviceName: userParams.deviceName,
-				page,
-			})
-
-			await simulateCreateProject({
-				page,
-				projectName: projectParams.projectName,
-			})
-
-			await page
-				.getByRole('navigation', { name: 'App navigation', exact: true })
-				.getByRole('button', {
-					name: `Go to project ${projectParams.projectName}.`,
-					exact: true,
-				})
-				.click()
-
-			// 2. Main tests
-			const button = page
-				.getByRole('navigation', { name: 'App navigation', exact: true })
-				.getByRole('button', {
-					name: `Show info for project ${projectParams.projectName}.`,
-					exact: true,
-				})
-
-			/// Click to toggle on behavior
-			await button.click()
-
-			const describedById = await button.getAttribute('aria-describedby')
-			expect(describedById).toBeTruthy()
-
-			const popup = page.locator(`[role="dialog"][id="${describedById}"]`)
-			await expect(popup).toBeVisible()
-			expect(
-				popup.getByRole('heading', {
-					name: projectParams.projectName,
-					exact: true,
-				}),
-			).toBeVisible()
-
-			//// Popup list items
-			const popupListItems = popup.getByRole('listitem')
-			await expect(popupListItems).toHaveCount(2)
-
-			const projectRoleItem = popupListItems.first()
-			await expect(projectRoleItem).toHaveText('Coordinator')
-
-			const categoriesSetItem = popupListItems.nth(1)
-			await expect(
-				categoriesSetItem.getByText(/^CoMapeo Default Categories \d\.\d$/),
-			).toBeVisible()
-			await expect(categoriesSetItem.getByText(/^Created .+$/)).toBeVisible()
-			// TODO: Ideally check for the actual values
-			const categoriesSetDateCreatedTime = categoriesSetItem.getByRole('time')
-			await expect(categoriesSetDateCreatedTime).not.toBeEmpty()
-			await expect(categoriesSetDateCreatedTime).toHaveAttribute('datetime')
-
-			/// Click to toggle off behavior
-			await button.click()
-			await expect(popup).not.toBeVisible()
-
-			/// Blur behavior
-			await button.blur()
-			await expect(popup).not.toBeVisible()
 		} finally {
 			// 3. Cleanup
 			await electronApp.close()
